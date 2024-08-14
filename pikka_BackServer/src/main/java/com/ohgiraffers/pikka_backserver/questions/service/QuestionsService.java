a3a231cc353ee896b242ef0bf9c8e51e05dfa505
Quokka <wotjr1142@gmail.com>
2024-08-14 14:18:50 +0900

    update QuestionsService

--- pikka_BackServer/src/main/java/com/ohgiraffers/pikka_backserver/questions/service/QuestionsService.java
+++ pikka_BackServer/src/main/java/com/ohgiraffers/pikka_backserver/questions/service/QuestionsService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,25 +27,30 @@ public class QuestionsService {
         return convertToDTO(questionsEntity);
     }
 
-    // 새로운 질문을 저장하는 메서드 추가
+    @Transactional
     public QuestionsDTO addQuestion(QuestionsDTO questionsDTO) {
         QuestionsEntity questionsEntity = convertToEntity(questionsDTO);
         QuestionsEntity savedEntity = questionsRepository.save(questionsEntity);
         return convertToDTO(savedEntity);
     }
 
+    @Transactional
     public QuestionsDTO submitAnswer(Integer contactId, QuestionsDTO questionsDTO) {
-        QuestionsEntity questionsEntity = questionsRepository.findById(contactId).orElse(null);
-        if (questionsEntity != null){
-            questionsEntity.setAnswerContent(questionsDTO.getAnswerContent());
-            questionsEntity.setResponsePostedDate(questionsDTO.getResponsePostedDate());
-            questionsEntity.setResponseStatus("완료");
-            questionsRepository.save(questionsEntity);
-        }
-        return convertToDTO(questionsEntity);
+        QuestionsEntity questionsEntity = questionsRepository.findById(contactId)
+                .orElseThrow(() -> new RuntimeException("Question not found with id: " + contactId));
+
+        questionsEntity.setAnswerContent(questionsDTO.getAnswerContent());
+        questionsEntity.setResponsePostedDate(questionsDTO.getResponsePostedDate());
+        questionsEntity.setResponseStatus("완료");
+
+        QuestionsEntity updatedEntity = questionsRepository.save(questionsEntity);
+        return convertToDTO(updatedEntity);
     }
 
-    public void deleteQuestion(Integer id){ questionsRepository.deleteById(id);}
+    public void deleteQuestion(Integer id){
+        questionsRepository.deleteById(id);
+    }
+
     public void deleteQna(Integer contactId) {
         questionsRepository.deleteById(contactId);
     }
@@ -60,6 +63,7 @@ public class QuestionsService {
         questionsDTO.setContactId(questionsEntity.getContactId());
         questionsDTO.setUserId(questionsEntity.getUserId());
         questionsDTO.setUserName(questionsEntity.getUserName());
+        questionsDTO.setUserTitle(questionsEntity.getUserTitle());
         questionsDTO.setContactType(questionsEntity.getContactType());
         questionsDTO.setContactContents(questionsEntity.getContactContents());
         questionsDTO.setContactPostedDate(questionsEntity.getContactPostedDate());
@@ -74,10 +78,14 @@ public class QuestionsService {
     }
 
     private QuestionsEntity convertToEntity(QuestionsDTO questionsDTO) {
+        if (questionsDTO == null) {
+            return null;
+        }
         QuestionsEntity questionsEntity = new QuestionsEntity();
         questionsEntity.setContactId(questionsDTO.getContactId());
         questionsEntity.setUserId(questionsDTO.getUserId());
         questionsEntity.setUserName(questionsDTO.getUserName());
+        questionsEntity.setUserTitle(questionsDTO.getUserTitle());
         questionsEntity.setContactType(questionsDTO.getContactType());
         questionsEntity.setContactContents(questionsDTO.getContactContents());
         questionsEntity.setContactPostedDate(questionsDTO.getContactPostedDate());
@@ -91,157 +99,64 @@ public class QuestionsService {
         return questionsEntity;
     }
 
-    // 유저 엔드포인트 서비스
     @Transactional
-    public Map<String, Integer> add(QuestionsDTO questionsDTO){
-        QuestionsEntity questionsEntity = new QuestionsEntity();
-        questionsEntity.setContactId(questionsDTO.getContactId());
-        questionsEntity.setUserId(questionsDTO.getUserId());
-        questionsEntity.setUserName(questionsDTO.getUserName());
-        questionsEntity.setUserTitle(questionsDTO.getUserTitle());
-        questionsEntity.setContactType(questionsDTO.getContactType());
-        questionsEntity.setContactContents(questionsDTO.getContactContents());
-        questionsEntity.setContactPostedDate(questionsDTO.getContactPostedDate());
-//        questionsEntity.setAdminId(questionsDTO.getAdminId());
-        questionsEntity.setAdminName(questionsDTO.getAdminName());
-        questionsEntity.setResponseTitle(questionsDTO.getResponseTitle());
-        questionsEntity.setResponseContents(questionsDTO.getResponseContents());
-        questionsEntity.setResponsePostedDate(questionsDTO.getResponsePostedDate());
-        questionsEntity.setResponseStatus(questionsDTO.getResponseStatus());
-        questionsEntity.setAnswerContent(questionsDTO.getAnswerContent());
-
-        QuestionsEntity savedInquiryEntity = questionsRepository.save(questionsEntity);
-
-        if (Objects.isNull(savedInquiryEntity)) {
-            return null;
-
-        }else {
-            return Map.of("postId", savedInquiryEntity.getUserId());
-        }
+    public QuestionsDTO add(QuestionsDTO questionsDTO){
+        QuestionsEntity questionsEntity = convertToEntity(questionsDTO);
+        QuestionsEntity savedEntity = questionsRepository.save(questionsEntity);
+        return convertToDTO(savedEntity);
     }
 
     public List<QuestionsDTO> getAllInquirys()  {
         List<QuestionsEntity> questionsEntities = questionsRepository.findAll();
         return questionsEntities.stream()
-                .map(questions -> {
-                    QuestionsDTO dto = new QuestionsDTO();
-                    dto.setContactId(questions.getContactId());
-                    dto.setUserId(questions.getUserId());
-                    dto.setUserName(questions.getUserName());
-                    dto.setUserTitle(questions.getUserTitle());
-                    dto.setContactType(questions.getContactType());
-                    dto.setContactContents(questions.getContactContents());
-                    dto.setContactPostedDate(questions.getContactPostedDate());
-//                    dto.setAdminId(questions.getAdminId());
-                    dto.setAdminName(questions.getAdminName());
-                    dto.setResponseTitle(questions.getResponseTitle());
-                    dto.setResponseContents(questions.getResponseContents());
-                    dto.setResponsePostedDate(questions.getResponsePostedDate());
-                    dto.setResponseStatus(questions.getResponseStatus());
-                    dto.setAnswerContent(questions.getAnswerContent());
-                    return dto;
-                })
+                .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
     public QuestionsDTO findInquiryById(Integer id) {
         QuestionsEntity questionsEntity = questionsRepository.findById(id).orElse(null);
-
-        if (questionsEntity == null) {
-            // 존재하지 않는 경우 null 반환
-            return null;
-        }
-        // DTO 객체를 직접 생성
-        QuestionsDTO questionsDTO = new QuestionsDTO();
-        questionsDTO.setContactId(questionsEntity.getContactId());
-        questionsDTO.setUserId(questionsEntity.getUserId());
-        questionsDTO.setUserName(questionsEntity.getUserName());
-        questionsDTO.setUserTitle(questionsEntity.getUserTitle());
-        questionsDTO.setContactType(questionsEntity.getContactType());
-        questionsDTO.setContactContents(questionsEntity.getContactContents());
-        questionsDTO.setContactPostedDate(questionsEntity.getContactPostedDate());
-//        questionsDTO.setAdminId(questionsEntity.getAdminId());
-        questionsDTO.setAdminName(questionsEntity.getAdminName());
-        questionsDTO.setResponseTitle(questionsEntity.getResponseTitle());
-        questionsDTO.setResponseContents(questionsEntity.getResponseContents());
-        questionsDTO.setResponsePostedDate(questionsEntity.getResponsePostedDate());
-        questionsDTO.setResponseStatus(questionsEntity.getResponseStatus());
-        questionsDTO.setAnswerContent(questionsEntity.getAnswerContent());
-
-        return questionsDTO;
+        return convertToDTO(questionsEntity);
     }
 
     public QuestionsDTO getInquiry(Integer id) {
-        QuestionsEntity questionsEntity = questionsRepository.findById(id).orElse(null);
-        if (questionsEntity == null) {
-            return null;
-        }
-
-        QuestionsDTO questionsDTO = new QuestionsDTO();
-        questionsDTO.setContactId(questionsEntity.getContactId());
-        questionsDTO.setUserId(questionsEntity.getUserId());
-        questionsDTO.setUserName(questionsEntity.getUserName());
-        questionsDTO.setUserTitle(questionsEntity.getUserTitle());
-        questionsDTO.setContactType(questionsEntity.getContactType());
-        questionsDTO.setContactContents(questionsEntity.getContactContents());
-        questionsDTO.setContactPostedDate(questionsEntity.getContactPostedDate());
-//        questionsDTO.setAdminId(questionsEntity.getAdminId());
-        questionsDTO.setAdminName(questionsEntity.getAdminName());
-        questionsDTO.setResponseTitle(questionsEntity.getResponseTitle());
-        questionsDTO.setResponseContents(questionsEntity.getResponseContents());
-        questionsDTO.setResponsePostedDate(questionsEntity.getResponsePostedDate());
-        questionsDTO.setResponseStatus(questionsEntity.getResponseStatus());
-        questionsDTO.setAnswerContent(questionsEntity.getAnswerContent());
-
-        return questionsDTO;
+        return findInquiryById(id);
     }
 
+    @Transactional
     public QuestionsDTO updateInquiry(Integer id, QuestionsDTO questionsDTO) {
-        QuestionsEntity questionsEntity = questionsRepository.findById(id).orElse(null);
-        if (questionsEntity == null) {
-            // 게시글이 존재하지 않으면 null 반환
-            return null;
+        QuestionsEntity questionsEntity = questionsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Inquiry not found with id: " + id));
+
+        System.out.println("Updating inquiry with id: " + id);
+        System.out.println("Current entity: " + questionsEntity);
+        System.out.println("Received DTO: " + questionsDTO);
+
+        // 업데이트할 필드들 설정
+        if (questionsDTO.getUserTitle() != null && !questionsDTO.getUserTitle().isEmpty()) {
+            questionsEntity.setUserTitle(questionsDTO.getUserTitle());
+            System.out.println("Updated userTitle to: " + questionsDTO.getUserTitle());
         }
 
-        // 기존 엔티티의 필드 수정
-        questionsEntity.setContactId(questionsDTO.getContactId());
-        questionsEntity.setUserId(questionsDTO.getUserId());
-        questionsEntity.setUserName(questionsDTO.getUserName());
-        questionsEntity.setUserTitle(questionsDTO.getUserTitle());
-        questionsEntity.setContactType(questionsDTO.getContactType());
-        questionsEntity.setContactContents(questionsDTO.getContactContents());
-        questionsEntity.setContactPostedDate(questionsDTO.getContactPostedDate());
-//        questionsEntity.setAdminId(questionsDTO.getAdminId());
-        questionsEntity.setAdminName(questionsDTO.getAdminName());
-        questionsEntity.setResponseTitle(questionsDTO.getResponseTitle());
-        questionsEntity.setResponseContents(questionsDTO.getResponseContents());
-        questionsEntity.setResponsePostedDate(questionsDTO.getResponsePostedDate());
-        questionsEntity.setResponseStatus(questionsDTO.getResponseStatus());
-        questionsEntity.setAnswerContent(questionsDTO.getAnswerContent());
+        // contactType 업데이트 (기존 값이 있어도 새 값으로 업데이트)
+        if (questionsDTO.getContactType() != null) {
+            String oldContactType = questionsEntity.getContactType();
+            questionsEntity.setContactType(questionsDTO.getContactType());
+            System.out.println("Updated contactType from '" + oldContactType + "' to '" + questionsDTO.getContactType() + "'");
+        }
 
-        // 엔티티 저장
-        QuestionsEntity updatedInquiryEntity = questionsRepository.save(questionsEntity);
+        if (questionsDTO.getContactContents() != null && !questionsDTO.getContactContents().isEmpty()) {
+            questionsEntity.setContactContents(questionsDTO.getContactContents());
+            System.out.println("Updated contactContents");
+        }
 
-        // DTO 객체 직접 생성
-        QuestionsDTO updatedInquiryDTO = new QuestionsDTO();
-        questionsDTO.setContactId(updatedInquiryEntity.getContactId());
-        questionsDTO.setUserId(updatedInquiryEntity.getUserId());
-        questionsDTO.setUserName(updatedInquiryEntity.getUserName());
-        questionsDTO.setUserTitle(updatedInquiryEntity.getUserTitle());
-        questionsDTO.setContactType(updatedInquiryEntity.getContactType());
-        questionsDTO.setContactContents(updatedInquiryEntity.getContactContents());
-        questionsDTO.setContactPostedDate(updatedInquiryEntity.getContactPostedDate());
-//        questionsDTO.setAdminId(updatedInquiryEntity.getAdminId());
-        questionsDTO.setAdminName(updatedInquiryEntity.getAdminName());
-        questionsDTO.setResponseTitle(updatedInquiryEntity.getResponseTitle());
-        questionsDTO.setResponseContents(updatedInquiryEntity.getResponseContents());
-        questionsDTO.setResponsePostedDate(updatedInquiryEntity.getResponsePostedDate());
-        questionsDTO.setResponseStatus(updatedInquiryEntity.getResponseStatus());
-        questionsDTO.setAnswerContent(updatedInquiryEntity.getAnswerContent());
+        // 필요한 다른 필드들도 업데이트
+        // ...
 
-        return updatedInquiryDTO;
+        QuestionsEntity updatedEntity = questionsRepository.save(questionsEntity);
+        System.out.println("Updated entity: " + updatedEntity);
+
+        return convertToDTO(updatedEntity);
     }
-
     public void deleteInquiryById(Integer id){
         questionsRepository.deleteById(id);
     }
